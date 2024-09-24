d0fbc6ce3bd6769f9a394caa3b1dfefeefc349bf
Shafanamar <149698834+Shafanamar@users.noreply.github.com>
2024-09-24 17:03:23 +0530

    index.js

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
+const { Client, GatewayIntentBits } = require('discord.js');
+const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+
+// Bot is ready
+client.once('ready', () => {
+    console.log('Bot is online!');
+});
+
+// Respond to messages
+client.on('messageCreate', message => {
+    if (message.content === '!ping') {
+        message.channel.send('Pong!');
+    }
+});
+
+// Log in to Discord with your token
+client.login('YOUR_BOT_TOKEN');
 
